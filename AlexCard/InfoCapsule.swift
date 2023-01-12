4d53d8d190b99bad0c85202fe3f7a11ecb7c8138
ADSkor <ADSkorotkin@gmail.com>
2023-01-13 01:36:50 +0700

    completed

---
+++ AlexCard/InfoCapsule.swift
@@ -0,0 +1,20 @@
+//
+//  InfoCapsule.swift
+//  AlexCard
+//
+//  Created by Aleksandr Skorotkin on 13.01.2023.
+//
+
+import SwiftUI
+
+struct InfoCapsule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoCapsule_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoCapsule()
+    }
+}
