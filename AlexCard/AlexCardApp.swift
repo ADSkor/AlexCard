6e55d93ea7035ecd44176d739b6475465f78afcb
ADSkor <ADSkorotkin@gmail.com>
2023-01-12 23:54:57 +0700

    Initial Commit

---
+++ AlexCard/AlexCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlexCardApp.swift
+//  AlexCard
+//
+//  Created by Aleksandr Skorotkin on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AlexCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
